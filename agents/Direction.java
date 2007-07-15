6dfe4ec821990386459d73dd05c80e12b53b0500
A.J.DeStefano <A.J.DeStefano@d41eb368-1a35-0410-9dc7-cfb72982dd03>
2007-07-15 05:37:08 +0000

    adding direction

---
+++ agents/Direction.java
@@ -0,0 +1,17 @@
+public enum Direction {
+
+    UP ('u'),
+    DOWN ('d'),
+    LEFT ('l'),
+    RIGHT ('r');
+    
+    private final byte direction;
+    
+    Direction (char direction) {
+	this.direction = (byte) direction;
+    }
+    
+    public byte getByte () { return direction; }
+
+}
+
