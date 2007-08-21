7b9856d0059cf38f33a0b73cc7971bfa825d6152
sean.busbey <sean.busbey@d41eb368-1a35-0410-9dc7-cfb72982dd03>
2007-08-21 04:17:08 +0000

    uploader works

--- submission/config-sample.php
+++ submission/config-sample.php
@@ -4,14 +4,17 @@
 
 	/** @brief allowed compression formats */
 	$uploadTypes = Array(
-		'POSIX tar archive',
-		'gzip compressed data, from Unix',
-		'POSIX tar archive (gzip compressed data, from Unix)',
-		'bzip2 compressed data, block size = 900k',
-		'POSIX tar archive (bzip2 compressed data, block size = 900k)',
-		'Zip archive data, at least v1.0 to extract',
+		'POSIX tar archive' => 'tar',
+		'gzip compressed data, from Unix'	=> 'tar.gz',
+		'POSIX tar archive (gzip compressed data, from Unix)'	=> 'tar.gz',
+		'bzip2 compressed data, block size = 900k'	=> 'tar.bz2',
+		'POSIX tar archive (bzip2 compressed data, block size = 900k)'	=> 'tar.bz2',
+		'Zip archive data, at least v1.0 to extract' => 'zip',
 	);
-	/** @brief command to use to check types */
-	$uploadCheck = '/usr/bin/file';
 	
+	/** @brief command to use to check types */
+	$uploadCheck = '/usr/bin/file -b -z';
+	
+	/** @brief placeholder for identity.  need to map user to teams */
+	$_SESSION['team'] = $_REQUEST['team'];
 ?>
