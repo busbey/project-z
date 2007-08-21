b34700646e9c97c153505d114a85f42eba1ec42a
sean.busbey <sean.busbey@d41eb368-1a35-0410-9dc7-cfb72982dd03>
2007-08-21 04:16:48 +0000

    uploader works

--- submission/index.php
+++ submission/index.php
@@ -2,16 +2,25 @@
 	require_once('config.php');
 	if(isset($_REQUEST['submit']))
 	{
-		$rawType = `$uploadCheck $_FILES['uploadedfile']['tmp_name']`;	
+		$check = $uploadCheck . ' ' . $_FILES['usersource']['tmp_name'];
+		$rawType = exec($check);	
 		if(array_key_exists($rawType, $uploadTypes))
 		{
 			$type = $uploadTypes[$rawType];
-			$storedfile = $uploadDir . '/' . $_SESSION['team']. $type;
-
+			$storedfile = $uploadDir . '/' . $_SESSION['team'].'.'.$type;
+			if(true === move_uploaded_file($_FILES['usersource']['tmp_name'], $storedfile))
+			{
+				echo "Successfully stored submission for ".$_SESSION['team'];
+			}
+			else
+			{
+				echo "Error handling upload: Couldn't move to destination.";
+				exit();
+			}
 		}
 		else
 		{
-			echo "Error handling upload: Unkown file type.";
+			echo "Error handling upload: Unkown file type. (".$check.' => '.$rawType.")";
 			exit();
 		}
 	}
@@ -19,8 +28,9 @@
 	{
 		?>
 <form enctype="multipart/form-data" action="index.php" method="POST">
-Choose your sumbission bundle: <input name="uploadedfile" type="file" /><br />
+Choose your sumbission bundle: <input name="usersource" type="file" /><br />
 <input type="submit" name="submit" value="Upload File" />
+<input type="hidden" name="team" value="<?php echo $_SESSION['team']; ?>" />
 </form>
 		<?php
 	}
