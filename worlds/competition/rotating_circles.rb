7479e0a3be491b0176d858184f997105df92595b
noah.easterly <noah.easterly@d41eb368-1a35-0410-9dc7-cfb72982dd03>
2007-10-31 11:00:27 +0000

    added smaller rotating circles

--- worlds/competition/rotating_circles.rb
+++ worlds/competition/rotating_circles.rb
@@ -1,15 +1,17 @@
 # automatically produce a rotating circle map using
 # rotating_circles.template
 
-all = ( (?a .. ?z).to_a + (?A .. ?R).to_a ).map { |i| i.chr }
-gaps = %w{ n o E D Q P }
+big = ( (?a .. ?z).to_a + (?A .. ?R).to_a ).map { |i| i.chr }
+small = ( (?S .. ?Z).to_a + (?0 .. ?7).to_a ).map { |i| i.chr }
+all = big + small
+gaps = %w{ n o E D Q P S T U}
 template = File.read 'rotating_circles.template'
 
-orig = all.join
-shif = ( all[1 .. -1] << all[0] ).join
+orig = big.join << small.join
+shif = ( big[1 .. -1] << big[0] ).join << ( small[1 .. -1] << small[0] ).join
 
 animation = [template]
-43.times { |i| animation[i+1] = animation[i].tr( orig, shif ) }
+175.times { |i| animation[i+1] = animation[i].tr( orig, shif ) }
 
 File.open( 'rotating_circles', "w") do |f|
 	f.puts ";rounds per frame:5"
